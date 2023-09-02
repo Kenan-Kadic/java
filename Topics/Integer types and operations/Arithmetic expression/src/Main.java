24c2663e92731f7afe4b7c45476bc13507dc51fb
Kenan Kadic <ken.kadic@outlook.com>
2023-09-02 17:28:56 -0500

    finished assignment

--- Topics/Integer types and operations/Arithmetic expression/src/Main.java
+++ Topics/Integer types and operations/Arithmetic expression/src/Main.java
@@ -4,5 +4,9 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        //System.out.println("Please enter a integer: ");
+        int n = scanner.nextInt();
+        System.out.println(((n + 1) * n + 2) * n + 3);
+        scanner.close();
     }
 }
