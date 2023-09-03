e617b46df758e445377d49c8a432bd569bbe0926
Kenan Kadic <ken.kadic@outlook.com>
2023-09-03 15:43:55 -0500

    coding problem solved

--- Topics/Integer types and operations/First digit/src/Main.java
+++ Topics/Integer types and operations/First digit/src/Main.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
-        System.out.println("please enter a digit: ");
+        //System.out.println("please enter a digit: ");
         int digit = scanner.nextInt();
 
         System.out.println(digit/10);
