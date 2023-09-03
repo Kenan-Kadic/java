d79c5b1c01a991543643d3be40fcd786db2214fc
Kenan Kadic <ken.kadic@outlook.com>
2023-09-03 15:42:41 -0500

    initial commit

---
+++ Topics/Integer types and operations/First digit/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        System.out.println("please enter a digit: ");
+        int digit = scanner.nextInt();
+
+        System.out.println(digit/10);
+    }
+}
