ecaa81037a6e0aeccd4a726d73b7d91d0838e80a
Kenan Kadic <ken.kadic@outlook.com>
2023-09-03 14:35:45 -0500

    coding challange solved

--- Topics/Integer types and operations/How many nuts each squirrel will get/src/Main.java
+++ Topics/Integer types and operations/How many nuts each squirrel will get/src/Main.java
@@ -4,5 +4,16 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        //System.out.println("Please enter the number of squirrels: ");
+        int numberOfSquirrels = scanner.nextInt();
+
+        //System.out.println("Please enter the number of nuts: ");
+        int numberOfNuts = scanner.nextInt();
+
+        scanner.close();
+
+        int nutsPerSquirrel = (numberOfNuts / numberOfSquirrels);
+
+        System.out.println(nutsPerSquirrel);
     }
 }
