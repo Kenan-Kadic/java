7fb25e8338480862b71cef8df67c48e078a1551a
Kenan Kadic <ken.kadic@outlook.com>
2023-09-04 09:45:44 -0500

    coding challnage solved

---
+++ Topics/Conditional statement/Symmetrical number/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        int firstDigit = number / 1000;
+        int secondDigit = (number % 1000) / 100;
+        int thirdDigit = (number % 100) / 10;
+        int fourthDigit = number % 10;
+
+        if (firstDigit == fourthDigit && secondDigit == thirdDigit) {
+            System.out.println("1");
+        } else {
+            System.out.println("2");
+        }
+    }
+}
