72681394d850727ec75a401aae8e00e5bf647ef8
Kenan Kadic <ken.kadic@outlook.com>
2023-09-06 16:06:19 -0500

    solution found

--- Topics/The while and do-while loops/The length of the sequence/src/Main.java
+++ Topics/The while and do-while loops/The length of the sequence/src/Main.java
@@ -1,8 +1,18 @@
-git aimport java.util.Scanner;
+import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // start coding here
+        int count = 0;
+
+        while (true) {
+            int num = scanner.nextInt();
+            if (num == 0) {
+                break;
+            }
+            count++;
+        }
+
+        System.out.println(count);
     }
 }
