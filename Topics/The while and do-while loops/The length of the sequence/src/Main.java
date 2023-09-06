64657428fb0e1fc3b00cdb7c7f0bda8f426c1e2b
Kenan Kadic <ken.kadic@outlook.com>
2023-09-06 15:13:33 -0500

    initial commit

---
+++ Topics/The while and do-while loops/The length of the sequence/src/Main.java
@@ -0,0 +1,8 @@
+git aimport java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+    }
+}
